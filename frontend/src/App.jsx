57946e9c0881a68073eb0ed38f5041352624d3bd
byLuisfer <luisferdass@gmail.com>
2025-01-16 15:50:40 +0100

    Frontend component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Home from './components/Home.jsx'
 
 function App() {
-  return <h1 className="text-red-500 text-9xl">Hello World</h1>
+  return <Home />
 }
 
 export default App
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <h1 className="text-5xl text-center font-bold">
+      Welcome to the IT-Workshop-Vorlage!
+    </h1>
+  )
+}
+
+export default Home
